65210ce41ef384f4f81cbe73eaf254732e54e7ef
Digi18 <me.digvijay18@gmail.com>
2019-04-25 13:34:10 +0530

    First push on Heroku

---
+++ routes/register.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const dotEnv = require('dotenv').config();
+
+var app = express();
+
+var dburl = process.env.URL;
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true}));
+
+router.post('/users', (req,res) => {
+
+     // res.send("Hello there");
+
+      var data = {
+
+          _id:req.body.id,
+          Name:req.body.username,
+          Email:req.body.email 
+      };
+
+      MongoClient.connect(dburl,{ useNewUrlParser: true }, (err,client) => {
+
+          
+             if(err){
+
+             	console.log('Unable to connect with database'.red +err);
+             }
+             else{
+
+             	client.db("Bookbudi_db").collection("Users").findOne({_id:req.body.id},function(err,user) {
+
+                          if(err){
+
+                            res.send("Error:" +err);
+                          	console.log("Error:".red +err);
+                          }
+                          if(user){
+
+                          	res.send("User exists");
+                          	console.log("user exists");
+                          }
+                          else{
+
+                          	var collection = client.db("Bookbudi_db").collection("Users");
+
+                          	collection.insertOne(data,(err,resp) => {
+
+                                 if(err){
+
+                                 	console.log("Insertion error".red +err);
+                                 	res.send("Error:" +err);
+                                 }
+                                 else{
+
+                                 	console.log("User created".green);
+                                 	res.send("User created");
+                                 }
+
+
+                          	});
+                               
+                         }
+                     
+                           client.close();
+             	});
+
+             }
+
+      });  
+
+
+
+});
+
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const color = require('colors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine','ejs');
+app.use(require('./routes/register'));
+
+const port = process.env.PORT || 3000; 
+
+app.listen(port, (req,res) => {
+
+    console.log("Server is running at: ".green, +port);
+
+});
+
+
+
+     
