3409657c94fcaa8bedd06e099818c20a671d36dc
Digi18 <me.digvijay18@gmail.com>
2019-06-15 18:32:43 +0530

    Some changes

--- routes/register.js
+++ routes/register.js
@@ -25,18 +25,13 @@ router.post('/users', (req,res) => {
   from: 'travotics1@gmail.com',
   templateId:'d-8e322d7da4f44ca1afc76aefb3721972',
   dynamic_template_data:{
-                    {
-                    Sender_Name: 'Bookbudi'
-                   },
-                   { Sender_Address: '971/32 alwar gate'
-                   },
-                   {Sender_City: 'Ajmer'
-                   },
-                   {Sender_State:'Rajasthan'
-                   },
-                   {Sender_Zip:'305001'
-                   }
-                }  
+                    
+                    Sender_Name: 'Bookbudi',
+                    Sender_Address: '971/32 alwar gate',
+                    Sender_City: 'Ajmer',
+                    Sender_State:'Rajasthan',
+                    Sender_Zip:'305001'
+                   }  
                
 // subject: 'Sending with Twilio SendGrid is Fun',
 //  text: 'and easy to do anywhere, even with Node.js',
