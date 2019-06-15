e3268d02b16abba2597199d8f2ebdff1c1468699
Digi18 <me.digvijay18@gmail.com>
2019-06-15 20:42:02 +0530

    Some changes

--- routes/register.js
+++ routes/register.js
@@ -26,7 +26,7 @@ router.post('/users', (req,res) => {
   to: req.body.email,
   from: 'travotics1@gmail.com',
   templateId:'d-8e322d7da4f44ca1afc76aefb3721972',
-  dynamicTemplateData: {
+  dynamic_Template_Data: {
           
           Sender_Name: "Bookbudi",
           Sender_City: "Ajmer",
