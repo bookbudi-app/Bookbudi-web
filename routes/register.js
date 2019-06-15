b235d5d86267a43c5996fecb82c4ae8fc3ecd1f4
Digi18 <me.digvijay18@gmail.com>
2019-06-15 19:32:56 +0530

    Some changes

--- routes/register.js
+++ routes/register.js
@@ -24,15 +24,15 @@ router.post('/users', (req,res) => {
   to: req.body.email,
   from: 'travotics1@gmail.com',
   templateId:'d-8e322d7da4f44ca1afc76aefb3721972',
- // substitutionWrapppers:['{{  }}'],
+  substitutionWrapppers:['{{  }}'],
   substitutions:
   {
 
-    "-company_name-":['Bookbudi'],
-    "-address-":['971/32 alwar gate'],
-    "-city-":['Ajmer'],
-    "-state-":['Rajasthan'],
-    "zip-":['305001']
+    company_name:'Bookbudi',
+    address:'971/32 alwar gate',
+    city:'Ajmer',
+    "-state-":'Rajasthan',
+    "zip-":'305001'
    }
   
  /* dynamic_template_data:{
