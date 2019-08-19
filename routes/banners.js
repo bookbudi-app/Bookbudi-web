d61f625e99b34db28ddf904be8d0611ec6a4c4ce
Digi18 <me.digvijay18@gmail.com>
2019-08-19 13:01:10 +0530

    Fixed bug

--- routes/banners.js
+++ routes/banners.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
@@ -43,49 +43,3 @@ router.get('/banners', (req,res) => {
     });
 
 module.exports = router;
-=======
-const express = require('express');
-const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const env = require('dotenv').config();
-
-var dburl = process.env.URL;
-
-router.get('/banners', (req,res) => {
-
-       MongoClient.connect(dburl, {useNewUrlParser:true}, (err,client) => {
-
-               if(err){
-
-               	console.log("Error".yellow, +err);
-               }
-               else{
-
-               	var collect = client.db('Bookbudi_db').collection('banners');
-
-               	collect.find({}).toArray((err,result) => {
-
-                      
-                      if(err){
-
-                      	console.log("Error:".red,+err);
-                      }
-                      else{
-
-                      	output = result.map(r => ({'Image':r.Image}));
-
-                      	res.json(output);
-
-                        client.close();
-
-                     }    
-               	});
-
-               }
-
-        });
-
-    });
-
-module.exports = router;
->>>>>>> 39e717597cc854f79ab6273d5bc3ab9a3cc87829
