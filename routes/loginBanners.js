d236239520988d85d40e7c91c52aea0527f68a9f
Digi18 <me.digvijay18@gmail.com>
2019-08-19 15:28:19 +0530

    fixed bugs

--- routes/loginBanners.js
+++ routes/loginBanners.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
@@ -46,52 +45,4 @@ router.get('/loginBanners', (req,res) =>{
 
 module.exports = router;
 
-=======
-const express = require('express');
-const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const env = require('dotenv').config();
 
-var dburl = process.env.URL;
-
-router.get('/loginBanners', (req,res) =>{
-	
-         MongoClient.connect(dburl,{useNewUrlParser:true},(err,client) => {
-
-                    if(err){
-
-                       console.log('Error:'.red, +err);
-                    }
-                    else{
-
-                        var collect = client.db('Bookbudi_db').collection('LoginBanners');
-
-                        collect.find({}).toArray((err,result) => {
-
-                        if(err){
-
-                      	    console.log("Error:".red,+err);
-                         }
-
-                         else{
-
-                            output = result.map(r => ({'Image':r.Image,'Description':r.Description}));
-
-                            res.json(output);
-
-                            client.close(); 
-
-                           }
-
-                        });
-
-
-                   }
-
-         });
-
-});
-
-module.exports = router;
-
->>>>>>> 39e717597cc854f79ab6273d5bc3ab9a3cc87829
